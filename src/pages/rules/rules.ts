ae1b217b9b622d0273c525e15841a716e01a3a5f
andreakpresley <andreapresley5@gmail.com>
2017-08-04 20:34:56 -0500

    initial page setup
    -renamed rules and settings pages
    -added buttons to home page, removed starter text

--- src/pages/rules/rules.ts
+++ src/pages/rules/rules.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-about',
+  templateUrl: 'rules.html'
 })
-export class ContactPage {
+export class RulesPage {
 
   constructor(public navCtrl: NavController) {
 
