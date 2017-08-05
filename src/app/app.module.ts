6d1b00d48767c001c1aa309087408d32a6be7624
andreakpresley <andreapresley5@gmail.com>
2017-08-05 11:30:59 -0500

    card-initial-setup
    -Added card page/files
    -Linked button on home page to card page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RulesPage } from '../pages/rules/rules';
 import { SettingsPage } from '../pages/settings/settings';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { CardPage } from '../pages/card/card';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     RulesPage,
     SettingsPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    CardPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     RulesPage,
     SettingsPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    CardPage
   ],
   providers: [
     StatusBar,
